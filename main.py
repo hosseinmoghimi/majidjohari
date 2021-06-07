2575aa94795a3e7a098bda3486a5eba65968d509
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2021-06-07 23:19:42 +0430

    atom1 to atom2

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ atom2=Atom(id=2,x=12,y=4,z=3,spin_x=1,spin_y=2,spin_z=3)
 
 print(atom1)
 print(atom2)
-print(f'        distanse : {atom1.distance(atom2)}')
-print(f'auto_correlation : {atom1.auto_correlation(atom2)}')
+print(f'            distanse atom1 to atom2: {atom1.distance(atom2)}')
+print(f'    auto_correlation atom1 by atom2: {atom1.auto_correlation(atom2)}')
 
 # output
         # id= 1 ,location = (0,0,0) , spins = (3,4,1)
         # id= 2 ,location = (12,4,3) , spins = (1,2,3)
-        #         distanse : 13.0
-        # auto_correlation : 14
+        #             distanse atom1 to atom2: 13.0
+        #     auto_correlation atom1 by atom2: 14
 
