b99da04617202ea218f6e4c1744bbfd4aabb817c
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2021-06-07 23:06:32 +0430

    add some files

---
+++ app/models.py
@@ -0,0 +1,51 @@
+import math
+class Location:
+    def __init__(self,*args, **kwargs):
+        self.x=0
+        self.y=0
+        self.z=0
+        if 'x' in kwargs:
+            self.x=kwargs['x']
+        if 'y' in kwargs:
+            self.y=kwargs['y']
+        if 'z' in kwargs:
+            self.z=kwargs['z']
+
+    def __str__(self):
+        return f"({self.x},{self.y},{self.z})"
+
+    def distance(self,location2):
+        r2=(self.x-location2.x)*(self.x-location2.x)+(self.y-location2.y)*(self.y-location2.y)+(self.z-location2.z)*(self.z-location2.z)
+        return math.sqrt(r2)
+    
+class Spin:
+    def __init__(self,*args, **kwargs):
+        self.spin_x=0
+        self.spin_y=0
+        self.spin_z=0
+        if 'spin_x' in kwargs:
+            self.spin_x=kwargs['spin_x']
+        if 'spin_y' in kwargs:
+            self.spin_y=kwargs['spin_y']
+        if 'spin_z' in kwargs:
+            self.spin_z=kwargs['spin_z']
+    def __str__(self):
+        return f"({self.spin_x},{self.spin_y},{self.spin_z})"
+    def scalar(self,spin2):
+        r=(self.spin_x*spin2.spin_x)+(self.spin_y*spin2.spin_y)+(self.spin_z*spin2.spin_z)
+        return r
+class Atom:
+    def __init__(self,*args, **kwargs):
+        self.id=0
+        if 'id' in kwargs:
+            self.id=kwargs['id']
+        self.location=Location(*args, **kwargs)
+        self.spin=Spin(*args, **kwargs)
+    
+    def distance(self,atom2):
+        return self.location.distance(atom2.location)
+    def scalar(self,atom2):
+        return self.spin.scalar(atom2.spin)
+    
+    def __str__(self):
+        return f"id= {self.id} ,location = {self.location} , spins = {self.spin}"
---
+++ main.py
@@ -0,0 +1,12 @@
+from app.models import Atom,Location,Spin
+R_MIN=0
+R_MAX=50
+
+
+atom1=Atom(id=1,x=0,y=0,z=0,spin_x=3,spin_y=4,spin_z=1)
+atom2=Atom(id=2,x=12,y=4,z=3,spin_x=1,spin_y=2,spin_z=3)
+
+print(atom1)
+print(atom2)
+print(f'distanse : {atom1.distance(atom2)}')
+print(f'  scalar : {atom1.scalar(atom2)}')
