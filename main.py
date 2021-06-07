b59902a710a62e4c1c5dbc77f1c02a1a2e1638dc
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2021-06-07 23:10:57 +0430

    check output

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ print(atom1)
 print(atom2)
 print(f'distanse : {atom1.distance(atom2)}')
 print(f'  scalar : {atom1.scalar(atom2)}')
+
+# output
+# id= 1 ,location = (0,0,0) , spins = (3,4,1)
+# id= 2 ,location = (12,4,3) , spins = (1,2,3)
+# distanse : 13.0
+#   scalar : 14
